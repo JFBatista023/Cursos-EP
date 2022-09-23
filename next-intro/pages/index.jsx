fc965ce6d6d3fcc2e09056f06486b7bd6e1bf00e
JFBatista023 <joaofilipeb045@email.com>
2022-09-23 15:38:00 -0300

    Fifth Commit

---
+++ next-intro/pages/index.jsx
@@ -0,0 +1,45 @@
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen;
+
+// export default function HomePage() {
+// // import React from 'react';
+//     return (
+// // import Link from '../src/components/Link';
+//         <div>
+// // import Footer from '../src/components/patterns/Footer';
+//             <Title as="h2">Alura Cases - Home</Title>
+
+//             <Link href="/faq">
+// // function Title({ children, as }) {
+//                 Ir para a página do FAQ
+// //     const Tag = as;
+//             </Link>
+// //     return (
+//         </div>
+// //         <React.Fragment>
+//     )
+// //             <Tag>
+// }
+// //                 {children}
+// //             </Tag>
+// //             <style jsx>{`
+// //                 ${Tag} {
+// //                     color: red;
+// //                 }
+// //             `}</style>
+// //         </React.Fragment>
+// //     );
+// // }
+
+// // export default function HomePage() {
+// //     return (
+// //         <div>
+// //             <Title as="h2">Alura Cases - Home</Title>
+// //             <Link href="/faq">
+// //                 Ir para a página do FAQ
+// //             </Link>
+// //             <Footer />
+// //         </div>
+// //     )
+// // }
